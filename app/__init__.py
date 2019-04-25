4471f5c4fd6d4fc2985fbaa66fd10a0c6966b316
yanzhilong <yanzhilongll@163.com>
2019-04-26 07:08:28 +0800

    版本: v 0.0.11 修改日期: 2019-04-26 修改人: yanzhilong 修改内容: 1. 修改app/__init__.py 增加模板地址配置 2. 修改app/manage.py 优化启动命令

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,9 @@ from app.extensions import config_extentions
 from app.main import config_blueprint
 
 
-def create_app(pro_name):
-    app = Flask(pro_name, instance_relative_config=True)
+def create_app(pro_name, template_folder):
+    app = Flask(pro_name, instance_relative_config=True,
+                template_folder=template_folder)
     app.config.from_object(Config)
     app.config.from_pyfile('config.py')
     config_extentions(app)
--- manage.py
+++ manage.py
@@ -3,19 +3,20 @@
 # @author yanzhilong
 # @description 启动app
 # @created 20190425
-from flask_script import Manager,Server
+from flask_script import Manager, Server
 from flask_migrate import MigrateCommand
 
 from app import create_app
 
 
-app = create_app('backup')
+app = create_app('backup', 'app/templates')
 manager = Manager(app)
-manager.add_command('runserver', Server(host=app.config['SERVER_HOST'], 
+manager.add_command('runserver', Server(host=app.config['SERVER_HOST'],
                                         port=app.config['SERVER_PORT'],
                                         use_debugger=app.config['DEBUG']),
-                                        threaded=True)
+                    threaded=True)
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
+    # app.run(host=app.config['SERVER_HOST'],port=app.config['SERVER_PORT'],debug=app.config['DEBUG'])
